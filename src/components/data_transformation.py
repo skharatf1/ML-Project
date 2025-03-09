858b9963ad6ef1bf0b902925d7bdddc486b74018
Sahil Kharat <skharatf1@gmail.com>
2025-03-09 10:26:58 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -107,5 +107,11 @@ class DataTransformation:
 
             )
 
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+
         except Exception as e:
             raise CustomException(e,sys)
